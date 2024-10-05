9e21a80213f2bfbeb9fdfae5bdf7edd86fc718a7
yasi <auroramarple@gmail.com>
2024-10-05 09:36:38 +0330

    final changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./dist/output.css",
+    "./index.html",
+    "./app.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
